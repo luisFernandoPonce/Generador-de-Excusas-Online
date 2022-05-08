a4841e055c333f141ebceb6c6dd54ccf18b8cb1d
Luis Ponce <luiferponce@gmail.com>
2022-05-08 00:34:23 +0000

    generador commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,33 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+let who = ["Mi perro", "Mi pajaro", "Mi abuela", "Mi tortuga"];
+let action = ["se comió", "pisó", "se trago", "rompio"];
+let what = ["la tarea", "el proyecto", "las llaves"];
+let when = [
+  "ayer",
+  "hoy en la mañana",
+  "al terminar",
+  "en la cena",
+  "mientras dormia"
+];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+function arregloRandom(arreglo) {
+  let max = arreglo.length;
+  let random = Math.random() * max;
+  random = Math.floor(random);
+  return arreglo[random];
+}
+
+function generarExcusa() {
+  return `${arregloRandom(who)} ${arregloRandom(action)} ${arregloRandom(
+    what
+  )} ${arregloRandom(when)}`;
+}
+
+console.log(excuse);
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let excusa = document.querySelector("#excuse");
+  excusa.innerHTML = generarExcusa();
+  document.getElementById("boton").addEventListener("click", function() {
+    excusa.innerHTML = `<h3>${generarExcusa()}</h3>`;
+  });
 };
